57eee9f01074a3f75872aedad696e517cd402543
Steve Rubin <nerdforlife@gmail.com>
2014-02-14 18:15:43 -0800

    add caching to song analysis

--- radiotool/composer/song.py
+++ radiotool/composer/song.py
@@ -1,3 +1,6 @@
+import hashlib
+import pickle
+import os
 
 from ..algorithms import librosa_analysis
 from track import Track
@@ -8,8 +11,11 @@ class Song(Track):
     Allows access to a musical analysis of the song.
     """
 
-    def __init__(self, fn, name="Song name"):
+    def __init__(self, fn, name="Song name", cache_dir=None, refresh_cache=False):
         self._analysis = None
+        self._checksum = None
+        self.refresh_cache = refresh_cache
+        self.cache_dir = cache_dir
         Track.__init__(self, fn, name)
 
     @property
@@ -19,6 +25,31 @@ class Song(Track):
         if self._analysis is not None:
             return self._analysis
 
-        self._analysis = librosa_analysis.analyze_frames(self.all_as_mono(), self.samplerate)
+        if self.cache_dir is not None:
+            path = os.path.join(self.cache_dir, self.checksum)
+            try:
+                if refresh_cache: raise IOError
+                with open(path + '.pickle', 'rb') as pickle_file:
+                    self._analysis = pickle.load(pickle_file)
+            except IOError:
+                self._analysis = librosa_analysis.analyze_frames(self.all_as_mono(), self.samplerate)
+                with open(path + '.pickle', 'wb') as pickle_file:
+                    pickle.dump(self._analysis, pickle_file, pickle.HIGHEST_PROTOCOL)
+        else:
+            self._analysis = librosa_analysis.analyze_frames(self.all_as_mono(), self.samplerate)
         return self._analysis
 
+    @property
+    def checksum(self):
+        if self._checksum is not None:
+            return self._checksum
+
+        block_size = 65536
+        hasher = hashlib.sha256()
+        with open(self.filename, 'rb') as f:
+            buf = f.read(block_size)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = f.read(block_size)
+        self._checksum = hasher.digest()
+        return self._checksum
