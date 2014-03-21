4780b29fa75282851719c32b219f1c100aa6d817
Steve Rubin <nerdforlife@gmail.com>
2014-03-21 14:20:51 -0700

    order of magnitude speedup when we can fit the full cost table in memory

--- radiotool/algorithms/build_table_full_backtrace.pyx
+++ radiotool/algorithms/build_table_full_backtrace.pyx
@@ -1,5 +1,5 @@
 #cython: infer_types=True
-#cython: boundscheck=True
+#cython: boundscheck=False
 #cython: wraparound=False
 #cython: profile=True
 #cython: cdivision=True
@@ -19,77 +19,7 @@ cdef struct Params:
     int all_full
 
 
-cdef void get_tc_column(double[:, :] tc, int column, double[:] tc_column, int backward, Params p):
-    cdef int tc_index = 0
-    cdef int beat_seg_i = 0
-    cdef int i, j
-
-    if column >= p.p0_full:
-        tc_index = p.p0 + (column - p.p0_full)
-    else:
-        tc_index = column % p.n_beats
-
-    if not backward:
-        for i in range(p.max_beats_with_padding):
-            for j in range(p.p0):
-                tc_column[i * p.n_beats + j] = tc[j, tc_index]
-
-        for i in range(p.p0_full, p.all_full):
-            tc_column[i] = tc[p.p0 + i - p.p0_full, tc_index]
-    else:
-        for i in range(p.max_beats_with_padding):
-            for j in range(p.p0):
-                tc_column[i * p.n_beats + j] = tc[tc_index, j]
-
-        for i in range(p.p0_full, p.all_full):
-            tc_column[i] = tc[tc_index, p.p0 + i - p.p0_full]
-
-    #--- CONSTRAINTS ---#
-    # * don't go to pause before minimum length music segment
-    if (column == p.p0_full) and (not backward):
-        for i in range(p.n_beats * p.min_beats):
-            tc_column[i] += p.pen_val
-    elif (column < p.n_beats * p.min_beats) and backward:
-        tc_column[p.p0_full] += p.pen_val
-
-    # * don't go to pause after maximum length music segment
-    if (column == p.p0_full) and (not backward):
-        for i in range(p.n_beats * p.max_beats, p.p0_full):
-            tc_column[i] += p.pen_val
-    elif (p.p0_full > column >= p.n_beats * p.max_beats) and backward:
-        tc_column[p.p0_full] += p.pen_val
-
-    # * after pause, don't go to non-first segment beat
-    if (p.n_beats <= column < p.p0_full) and (not backward):
-        for i in range(p.p0_full, p.all_full):
-            tc_column[i] += p.pen_val
-    elif (column >= p.p0_full) and backward:
-        for i in range(p.n_beats, p.p0_full):
-            tc_column[i] += p.pen_val
-
-    # * don't move between beats the don't follow segment index
-    if column < p.p0_full:
-        for i in range(p.p0_full):
-            tc_column[i] += p.pen_val
-
-        beat_seg_i = column / p.n_beats
-
-        if (beat_seg_i > 0) and (not backward):
-            for i in range((beat_seg_i - 1) * p.n_beats, beat_seg_i * p.n_beats):
-                tc_column[i] -= p.pen_val
-
-        elif (beat_seg_i < p.max_beats_with_padding - 1) and backward:
-            for i in range((beat_seg_i + 1) * p.n_beats, (beat_seg_i + 2) * p.n_beats):
-                tc_column[i] -= p.pen_val
-
-        # you're also allowed to move infinitely among the
-        # last beat if max_beats is not set (== -1)
-        if p.max_beats == -1 and (beat_seg_i == p.min_beats):
-            for i in range(beat_seg_i * p.n_beats, (beat_seg_i + 1) * p.n_beats):
-                tc_column[i] -= p.pen_val
-
-
-cdef double get_pen_value(double[:, :] pen, int i, int l, int global_start_l, Params p):
+cdef double get_pen_value(double[:, :] pen, int i, int l, int global_start_l, Params p) nogil:
     cdef int pen_index = 0
     if i >= p.p0_full:
         pen_index = p.n_beats + (i - p.p0_full)
@@ -105,7 +35,7 @@ cdef double get_pen_value(double[:, :] pen, int i, int l, int global_start_l, Pa
     return new_pen
 
 
-cdef void get_pen_column(double[:, :] pen, int column, double[:] new_pen, int global_start_l, Params p):
+cdef void get_pen_column(double[:, :] pen, int column, double[:] new_pen, int global_start_l, Params p) nogil:
     cdef int i, j
 
     for i in range(p.max_beats_with_padding):
@@ -122,9 +52,10 @@ cdef void get_pen_column(double[:, :] pen, int column, double[:] new_pen, int gl
             new_pen[i] += p.pen_val
 
 
-cdef void backward_space_efficient_cost_with_duration_constraint(
+cdef void backward_build_table(
     double[:, :] tc, double[:, :] pen, int start_beat, int end_beat, int global_start_l, Params p,
-    double[:] cost, double[:] pen_val, double[:] vals_col, double[:] min_vals, int[:] global_path):
+    double[:] cost, double[:] pen_val, double[:] vals_col, double[:] min_vals, int[:] global_path,
+    double[:] global_path_cost) nogil:
     
     cdef int l, idx, i, beat_seg_i, seg_start_beat, j, full_j, orig_beat_j, min_idx
     cdef double minval, tmpval
@@ -132,17 +63,10 @@ cdef void backward_space_efficient_cost_with_duration_constraint(
     cdef double *full_cost = <double *>malloc(p.all_full * pen.shape[1] * sizeof(double))
     cdef int *prev_node = <int *>malloc(p.all_full * pen.shape[1] * sizeof(int))
 
-    print "full cost size", p.all_full, "x", pen.shape[1], "=", p.all_full * pen.shape[1]
+    # print "full cost size", p.all_full, "x", pen.shape[1], "=", p.all_full * pen.shape[1]
     
-    if not full_cost:
-        raise MemoryError()
-    if not prev_node:
-        raise MemoryError()
-
     min_idx = 0
-    cdef rowlen = pen.shape[1]
-
-    print "1"
+    cdef int rowlen = pen.shape[1]
 
     # generate initial cost
     if end_beat != -1:
@@ -152,22 +76,18 @@ cdef void backward_space_efficient_cost_with_duration_constraint(
         get_pen_column(pen, pen.shape[1] - 1, cost, global_start_l + pen.shape[1] - 1, p)
 
     for i in range(cost.shape[0]):
-        print i, i * rowlen + (pen.shape[1] - 1)
+        # print i, i * rowlen + (pen.shape[1] - 1)
         full_cost[i * rowlen + (pen.shape[1] - 1)] = cost[i]
         # ??
         # prev_node[i, pen.shape[1] - 1] = 0
 
-    print "2"
-
     # optimize
     # should this be  -2 or -1?
-    for l in xrange(pen.shape[1] - 2, 0, -1):
-        print "l", l
-
+    for l in xrange(pen.shape[1] - 2, -1, -1):
         get_pen_column(pen, l, pen_val, global_start_l + l, p)
 
         # categories of beats we could be at before this one
-        print "a"
+
         # beat segment before min_beat
         for idx in range(p.n_beats * (p.min_beats - 1)):
             beat_seg_i = idx / p.n_beats
@@ -186,7 +106,7 @@ cdef void backward_space_efficient_cost_with_duration_constraint(
 
             full_cost[idx * rowlen + l] = minval
             prev_node[idx * rowlen + l] = min_idx
-        print "b"
+
         # beat segment between min beat and max beat
         for idx in range(p.n_beats * (p.min_beats - 1), p.n_beats * (p.max_beats - 1)):
             beat_seg_i = idx / p.n_beats
@@ -210,7 +130,7 @@ cdef void backward_space_efficient_cost_with_duration_constraint(
 
             full_cost[idx * rowlen + l] = minval
             prev_node[idx * rowlen + l] = min_idx
-        print "c"
+
         # max beat segment
         for idx in range(p.n_beats * (p.max_beats - 1), p.n_beats * p.max_beats):
             orig_beat_i = idx % p.n_beats
@@ -220,7 +140,7 @@ cdef void backward_space_efficient_cost_with_duration_constraint(
 
             full_cost[idx * rowlen + l] = tc[orig_beat_i, p.p0] + pen_val[idx] + full_cost[p.p0_full * rowlen + (l + 1)]
             prev_node[idx * rowlen + l] = p.p0_full
-        print "d"
+
         # pause beats except the last one
         for idx in range(p.p0_full, p.all_full - 1):
             orig_beat_i = p.p0 + (idx - p.p0_full)
@@ -236,7 +156,7 @@ cdef void backward_space_efficient_cost_with_duration_constraint(
 
             full_cost[idx * rowlen + l] = minval
             prev_node[idx * rowlen + l] = min_idx
-        print "e"
+
         # last pause beat
         minval = -1
         for j in range(p.n_beats):
@@ -253,28 +173,32 @@ cdef void backward_space_efficient_cost_with_duration_constraint(
 
     # find the optimal path
 
-    # in the first column
-    print "prev node 50, 50"
-    print prev_node[50 * rowlen + 50]
+    # find start node
+    minval = -1
+    for i in range(p.all_full):
+        # print i, full_cost[i * rowlen + 0]
+        if minval == -1 or full_cost[i * rowlen + 0] < minval:
+            minval = full_cost[i * rowlen + 0]
+            min_idx = i
+    global_path[0] = min_idx
 
+    cdef double total_cost_remaining = minval
+    cdef int node = min_idx
+
+    for l in range(pen.shape[1] - 1):
+        node = prev_node[node * rowlen + l]
+        global_path[l + 1] = node
+        global_path_cost[l] = total_cost_remaining - full_cost[node * rowlen + (l + 1)]
+        total_cost_remaining -= global_path_cost[l]
+
+    # not sure what to grab for the initial cost
+    global_path_cost[pen.shape[1] - 1] = total_cost_remaining
 
     free( full_cost )
     free( prev_node )
 
 
-cdef void divide_and_conquer_cost_and_path(
-    double[:, :] tc, double[:, :] pen, int start_beat, int end_beat, int offset,
-    int[:] global_path, Params p,
-    double[:] f, double[:] g, double[:] mv1, double[:] mv2,
-    double[:] mv3, double[:] mv4, double[:] mv5, double[:] mv6):
-
-    backward_space_efficient_cost_with_duration_constraint(
-        tc, pen, -1, end_beat, 0, p, g, mv4, mv5, mv6, global_path)
-
-    return
-
-
-cpdef int[:] build_table(double[:, :] trans_cost, double[:, :] penalty,
+def build_table(double[:, :] trans_cost, double[:, :] penalty,
     int min_beats=-1, int max_beats=-1, int first_pause=-1):
     
     cdef int max_beats_with_padding, i
@@ -306,30 +230,30 @@ cpdef int[:] build_table(double[:, :] trans_cost, double[:, :] penalty,
 
     # double arrays for use throughout the computation
     cdef array dtemplate = array('d')
-    cdef array array1, array2, array3, array4, array5, array6, array7, array8
-    cdef double[:] mv1, mv2, mv3, mv4, mv5, mv6, f, g
+    cdef array array1, array2, array3, array4, array5
+    cdef double[:] mv1, mv2, mv3, mv4, global_path_cost
     array1 = clone(dtemplate, p.all_full, False)
     array2 = clone(dtemplate, p.all_full, False)
     array3 = clone(dtemplate, p.all_full, False)
     array4 = clone(dtemplate, p.all_full, False)
-    array5 = clone(dtemplate, p.all_full, False)
-    array6 = clone(dtemplate, p.all_full, False)
-    array7 = clone(dtemplate, p.all_full, False)
-    array8 = clone(dtemplate, p.all_full, False)
-    f = array1
-    g = array2
-    mv1 = array3
-    mv2 = array4
-    mv3 = array5
-    mv4 = array6
-    mv5 = array7
-    mv6 = array8
+    mv1 = array1
+    mv2 = array2
+    mv3 = array3
+    mv4 = array4
+
+    array5 = clone(dtemplate, penalty.shape[1], False)
+    global_path_cost = array5
 
     cdef array ar, template = array('i')
     ar = clone(template, penalty.shape[1], False)
     cdef int[:] global_path = ar
 
-    divide_and_conquer_cost_and_path(trans_cost, penalty, -1, -1, 0, global_path, p,
-        f, g, mv1, mv2, mv3, mv4, mv5, mv6)
+    backward_build_table(
+        trans_cost, penalty, -1, -1, 0, p, mv1, mv2, mv3, mv4, global_path, global_path_cost)
 
-    return global_path
+    # divide_and_conquer_cost_and_path(trans_cost, penalty, -1, -1, 0, global_path, p,
+    #     f, g, mv1, mv2, mv3, mv4, mv5, mv6)
+
+    
+
+    return [x for x in global_path], [x for x in global_path_cost]
