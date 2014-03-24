7738468cbbc3fca95b7a405cb77053de4b87884f
Steve Rubin <nerdforlife@gmail.com>
2014-03-24 15:25:51 -0700

    bug fix in recursive import

--- radiotool/composer/__init__.py
+++ radiotool/composer/__init__.py
@@ -6,16 +6,16 @@
 
 """
 
+from .composition import Composition
 from .track import Track
 from .rawtrack import RawTrack
 from .speech import Speech
-from .song import Song
 from .segment import Segment
 from .timestretchsegment import TimeStretchSegment
 from .dynamic import Dynamic
 from .fade import Fade
 from .volume import Volume
 from .rawvolume import RawVolume
-from .composition import Composition
 from .label import Label
+from .song import Song
 from .volumebreakpoint import VolumeBreakpoint, VolumeBreakpoints
