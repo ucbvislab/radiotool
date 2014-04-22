846e72f210bbc44d1335041ed316b91c4c6cfdf6
Steve Rubin <nerdforlife@gmail.com>
2014-04-22 16:06:39 -0700

    setup update

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from Cython.Distutils import build_ext
 import os
 import platform
 if platform.system() == "Darwin":
-      # openmp doesn't work with clang, the default OSX C compiler
-      os.environ["CC"] = "gcc-4.2"
-      os.environ["LDSHARED"] = "gcc-4.2"
-      os.environ["OMP_NUM_THREADS"] = "4"
-      os.environ["LDFLAGS"] = "-shared -lpython2.7 -I/usr/include/python2.7/"
+    # openmp doesn't work with clang, the default OSX C compiler
+    os.environ["CC"] = "gcc-4.2"
+    os.environ["LDSHARED"] = "gcc-4.2"
+    os.environ["OMP_NUM_THREADS"] = "4"
+    os.environ["LDFLAGS"] = "-shared -lpython2.7 -I/usr/include/python2.7/"
 
 
 
@@ -24,36 +24,35 @@ build_table_full_backtrace = Extension('radiotool.algorithms.build_table_full_ba
                                       ['radiotool/algorithms/build_table_full_backtrace.pyx'],
                                       extra_compile_args=['-O3'])
 
-setup(name='radiotool',
-      version='0.3.3',
-      description='Tools for constructing and retargeting audio',
-      long_description=open('README.rst').read(),
-      url='https://github.com/ucbvislab/radiotool',
-      author='Steve Rubin',
-      author_email='srubin@cs.berkeley.edu',
-      packages=[
-            'radiotool',
-            'radiotool.composer',
-            'radiotool.algorithms'
-      ],
-      ext_modules = [build_table_mem_efficient, par_build_table, build_table_full_backtrace],
-      cmdclass = {'build_ext': build_ext},
-      script_args = ['build_ext', '--inplace'],
-      license='GPL v3',
-      install_requires=[
-            'numpy',
-            'scipy',
-            'scikits.audiolab',
-            'librosa'
-      ],
-#      extras_require=[
-#            'python-xmp-toolkit'
-#      ],
-      # dependency_links=['https://github.com/bmcfee/librosa/tarball/master#egg=librosa-0.1'],
-      dependency_links=[],
-      zip_safe=False,
-      test_suite='nose.collector',
-      tests_require=['nose'])
+setup(
+    name='radiotool',
+    version='0.3.3',
+    description='Tools for constructing and retargeting audio',
+    long_description=open('README.rst').read(),
+    url='https://github.com/ucbvislab/radiotool',
+    author='Steve Rubin',
+    author_email='srubin@cs.berkeley.edu',
+    packages=[
+        'radiotool',
+        'radiotool.composer',
+        'radiotool.algorithms'
+    ],
+    ext_modules = [build_table_mem_efficient, par_build_table, build_table_full_backtrace],
+    cmdclass = {'build_ext': build_ext},
+    script_args = ['build_ext', '--inplace'],
+    license='GPL v3',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikits.audiolab',
+        'librosa'
+    ],
+    #      extras_require=[
+    #            'python-xmp-toolkit'
+    #      ],
+    zip_safe=False,
+    test_suite='nose.collector',
+    tests_require=['nose'])
 
 
 # We don't need the fortran version. The other version is fast enough (probably)
