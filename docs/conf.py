77b7fafed3119520804df9b1ee9fc355f7ae937b
Steve Rubin <nerdforlife@gmail.com>
2014-12-04 10:41:24 -0800

    trying to make rtd work

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,18 @@
 
 import sys, os
 
+
+# mock for readthedocks
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'sckits.audiolab', 'librosa', 'Cython']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
