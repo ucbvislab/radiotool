87e883cbfdec60062ee5355f20644ac0451a8f94
Steve Rubin <nerdforlife@gmail.com>
2014-12-04 15:54:51 -0800

    testing rtd again

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
         return Mock()
 
 MOCK_MODULES = ['numpy', 'scipy', 'scikits.audiolab',
-                'librosa', 'Cython', 'scipy.signal',
+                'librosa', 'scipy.signal',
                 'scipy.spatial.distance', 'scipy.linalg',
                 'scipy.special']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
